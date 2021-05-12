dbdd915ee1125ed1e1a5b382c0d8a464691ade8c
w2ptr <43069332+w2ptr@users.noreply.github.com>
2021-05-12 11:51:29 +0200

    Move ArcDiagram to correct folder

--- src/views/Visualise.vue
+++ src/views/Visualise.vue
@@ -1,22 +1,3 @@
 <template>
-  <arc-diagram></arc-diagram>
+  <h1></h1>
 </template>
-
-<!--<style>
-  body {
-    margin: 0;
-    padding: 0;
-    height: 100%;
-    width: 100%;
-    overflow: hidden;
-  }
-</style>-->
-
-<script lang="ts">
-import { defineComponent } from "vue";
-import ArcDiagram from "@/components/ArcDiagram.vue";
-export default defineComponent({
-  name: "Visualise",
-  components: { ArcDiagram },
-});
-</script>
