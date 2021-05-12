fe25d5921ff33b9ebdbaed64cbd860b48ccbe541
Nidhish <55269918+nidhishs@users.noreply.github.com>
2021-05-13 00:59:05 +0200

    feat: v1 of upload interface.

---
+++ src/components/visualise/NewVis.vue
@@ -0,0 +1,74 @@
+<template>
+  <div
+    tabindex="0"
+    @click="selectDiagram"
+    @keypress.enter="selectDiagram"
+    class="vis-tile"
+  >
+    <div class="vis-tile__bg">
+      <img :src="vis_type.icon" :alt="vis_type.name" class="vis-tile__icon" />
+    </div>
+    <h4 class="vis-tile__title">{{ vis_type.name }}</h4>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import store from "@/scripts/store";
+
+export default defineComponent({
+  name: "NewVis",
+  props: {
+    vis_type: { required: true, type: Object },
+  },
+  methods: {
+    selectDiagram() {
+      this.$emit('tile-click');
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/styles/_config.scss";
+@include font-face("Poppins", "Medium");
+
+.vis-tile {
+  text-align: center;
+  width: 120px;
+  margin: 10px;
+  cursor: pointer;
+
+  &:hover,
+  &:focus {
+    outline: none;
+    .vis-tile__bg {
+      transform: scale(1.05);
+    }
+  }
+
+  &:focus &__bg {
+    border: 1px solid $WHITE;
+  }
+
+  &__bg {
+    @include setSize(120px, 120px);
+    background-color: $BLACK_L;
+    padding: 5px;
+    border-radius: 5px;
+    overflow: hidden;
+    margin-bottom: 5px;
+    box-shadow: 2px 2px 4px rgba($BLACK_DDD, 0.25);
+    @include transition(transform border, 0.3s, $ease2);
+
+    .vis-tile__icon {
+      max-width: 100%;
+      max-height: 100%;
+    }
+  }
+
+  &__title {
+    @include font-sans("Poppins", 0.8rem, "Medium", $BLACK_DDD);
+  }
+}
+</style>
---
+++ src/components/visualise/UploadDataset.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="upload-panel">
+    <span class="upload-panel__back" @click="$emit('back')">BACK</span>
+    <label tabindex="0" class="upload-panel__btn" @change="addDataset">
+      <input type="file" accept="text/csv" />
+      UPLOAD
+    </label>
+    <h5 class="upload-panel__hint">Upload your dataset here.</h5>
+    <h6 class="upload-panel__sub-hint">
+      Note: The dataset must be in a CSV format.
+    </h6>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "UploadDataset",
+  methods: {
+    addDataset(event: { target: { files: File[] } }) {
+      console.log(event);
+      this.$emit("back");
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/styles/_config.scss";
+@include font-face("Poppins", "Medium");
+@include font-face("Poppins", "Regular");
+.upload-panel {
+  height: 100%;
+  background-color: $WHITE;
+
+  // Center the button and hints.
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  position: relative;
+
+  &__back {
+    @include font-sans("Poppins", 0.75rem, "Regular", $BLACK_DDD);
+    @include abs(30px 0 0 30px);
+    cursor: pointer;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  &__btn {
+    input[type="file"] {
+      display: none;
+    }
+
+    @include center_item();
+    @include font-sans("Poppins", 1.25rem, "Medium", $BLACK_DDD);
+    position: relative;
+    margin-bottom: 15px;
+    @include transition(background color, 0.25s, $ease1);
+    @include setSize(200px, 50px);
+
+    &:focus,
+    &:hover {
+      // Remove default outline on focus.
+      outline: none;
+      background-color: $AQUAMARINE;
+      color: $WHITE;
+    }
+
+    // Give a button like effect.
+    &::before {
+      @include pseudo($width: 100%, $height: 100%);
+      border-radius: 5px;
+      border: 2px solid $AQUAMARINE;
+    }
+  }
+
+  &__hint {
+    @include font-sans("Poppins", 0.9rem, "Regular", $BLACK_DDD);
+  }
+
+  &__sub-hint {
+    @include font-sans("Poppins", 0.75rem, "Regular", $GREY);
+  }
+}
+</style>
