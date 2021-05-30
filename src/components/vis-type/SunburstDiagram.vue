b5384e79f554f7d8548c7fe7470e305c265661eb
Stijn <6910589+Stijn-H@users.noreply.github.com>
2021-05-30 16:45:59 +0200

    Reset root to clicked node.
    When a node is clicked, this node will be set as the root and the diagram will be redrawn from the new root.

--- src/components/vis-type/SunburstDiagram.vue
+++ src/components/vis-type/SunburstDiagram.vue
@@ -10,6 +10,22 @@ import * as d3 from "d3";
 import * as PIXI from "pixi.js";
 import * as GlobalStorage from "@/scripts/globalstorage";
 
+var graph = new Graph();
+var app;
+
+// Create map for number of connections between nodes
+var bothConnections = new Map();
+var outConnections = new Map();
+
+const input = {
+  root: false, // false or root index
+  height: 5, // >= 0
+  graphType: "sunburst", // sunburst or flame or inverse-flame
+  edgeType: "all", // all or incoming or outgoing
+  widthType: "connections", // connections or subtree-size TODO
+  minRenderSize: 0.001, // minimum render size
+};
+
 // stolen from https://pixijs.download/dev/docs/packages_graphics-extras_src_drawTorus.ts.html
 function drawTorus(graphics: PIXI.Graphics,
     x: number,
@@ -49,7 +65,7 @@ export default defineComponent({
       console.warn("Non-existent diagram:", this.diagramid);
       return;
     }
-    const graph = GlobalStorage.getDataset(diagram.graphID);
+    graph = (GlobalStorage.getDataset(diagram.graphID) as Graph);
     if (!graph) {
       console.warn("Non-existent dataset:", diagram.graphID);
       return;
@@ -68,19 +84,6 @@ export default defineComponent({
     const defaultStyle = new PIXI.TextStyle({
       fill: "#000000",
     });
-
-    const input = {
-      root: false, // false or root index
-      height: 5, // >= 0
-      graphType: "flame", // sunburst or flame or inverse-flame
-      edgeType: "all", // all or incoming or outgoing
-      widthType: "connections", // connections or subtree-size TODO
-      minRenderSize: 0.001, // minimum render size
-    };
-
-    // Create map for number of connections between nodes
-    var bothConnections = new Map();
-    var outConnections = new Map();
 
     if (input.widthType == "connections") {
       [bothConnections, outConnections] = this.mapConnections(graph);
@@ -120,8 +123,7 @@ export default defineComponent({
       const canvas = this.$refs["drawing-canvas"] as HTMLCanvasElement;
       const tooltip = this.$refs["graph-tooltip"] as HTMLElement;
 
-      const graphics = new PIXI.Graphics();
-      app.stage.addChild(graphics);
+      app.stage.removeChildren();
 
       canvas.height = window.innerHeight;
       canvas.width = window.innerWidth;
@@ -223,18 +225,9 @@ export default defineComponent({
 
         // Draw node
         if (graphType == "sunburst") {
-          var startAngle = 2 * Math.PI * drawStart;
-          var endAngle = 2 * Math.PI * (drawStart + sizePerc);
-          this.drawNodeSunburst(app, centerX, centerY, level, levelHeight, startAngle, endAngle, subtreeColour);
+          this.drawNodeSunburst(app, node, centerX, centerY, level, levelHeight, drawStart, sizePerc, subtreeColour);
         } else {
-          var posX = centerX - (maxWidth / 2) + (maxWidth * drawStart);
-          var posY;
-          if (graphType == "flame") {
-            posY = centerY + ((levelHeight * (height + 1)) / 2) - (levelHeight * level);
-          } else {
-            posY = centerY - ((levelHeight * (height + 1)) / 2) + (levelHeight * level);
-          }
-          this.drawNodeFlame(app, graphType, posX, posY, maxWidth * sizePerc, levelHeight, subtreeColour);
+          this.drawNodeFlame(app, graphType, node, centerX, centerY, maxWidth, drawStart, sizePerc, height, level, levelHeight, subtreeColour);
         }
 
         // Add this node to copy of predecessors
@@ -308,8 +301,10 @@ export default defineComponent({
   },
 
   // Draw node for sunburst graph
-  drawNodeSunburst(app: PIXI.Application, centerX: any, centerY: any, level: any, levelHeight: any, startAngle: any, endAngle: any, nodeColour: any) {
+  drawNodeSunburst(app: PIXI.Application, node: any, centerX: any, centerY: any, level: any, levelHeight: any, drawStart: any, sizePerc: any, nodeColour: any) {
 
+    var startAngle = 2 * Math.PI * drawStart;
+    var endAngle = 2 * Math.PI * (drawStart + sizePerc);
     var minRadius = level * levelHeight;
     var maxRadius = minRadius + levelHeight;
 
@@ -324,10 +319,33 @@ export default defineComponent({
     }
     sunburstNode.endFill();
     app.stage.addChild(sunburstNode);
+
+    // Interactivity
+    sunburstNode.interactive = true;
+    sunburstNode.buttonMode = true;
+    sunburstNode.on('pointerdown', (event) => {
+      event.stopPropagation();
+
+      // Reset graph is the user presses the node in the middle
+      if (level == 0) {
+        this.draw(graph, app, false, input.height, input.graphType, input.edgeType, input.minRenderSize, bothConnections, outConnections);
+      } else {
+        this.draw(graph, app, node, input.height, input.graphType, input.edgeType, input.minRenderSize, bothConnections, outConnections);
+      }
+    });
   },
 
   // Draw node for flame graph
-  drawNodeFlame(app: PIXI.Application, graphType: any, posX: any, posY: any, levelWidth: any, levelHeight: any, nodeColour: any) {
+  drawNodeFlame(app: PIXI.Application, graphType: any, node: any, centerX: any, centerY: any, maxWidth: any, drawStart: any, sizePerc: any, height: any, level: any, levelHeight: any, nodeColour: any) {
+    var posX = centerX - (maxWidth / 2) + (maxWidth * drawStart);
+    var posY;
+    if (graphType == "flame") {
+      posY = centerY + ((levelHeight * (height + 1)) / 2) - (levelHeight * level);
+    } else {
+      posY = centerY - ((levelHeight * (height + 1)) / 2) + (levelHeight * level);
+    }
+    var levelWidth = maxWidth * sizePerc;
+
     const flameNode = new PIXI.Graphics();
 
     flameNode.beginFill(nodeColour);
@@ -339,6 +357,20 @@ export default defineComponent({
     }
     flameNode.endFill();
     app.stage.addChild(flameNode);
+
+    // Interactivity
+    flameNode.interactive = true;
+    flameNode.buttonMode = true;
+    flameNode.on('pointerdown', (event) => {
+      event.stopPropagation();
+
+      // Reset graph is the user presses the node in the middle
+      if (level == 0) {
+        this.draw(graph, app, false, input.height, input.graphType, input.edgeType, input.minRenderSize, bothConnections, outConnections);
+      } else {
+        this.draw(graph, app, node, input.height, input.graphType, input.edgeType, input.minRenderSize, bothConnections, outConnections);
+      }
+    });
   }
 },
 });
