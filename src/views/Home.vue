5104ecd0aff1caa7532067456a4a0952b0197d44
chriscross29 <82936808+chriscross29@users.noreply.github.com>
2021-06-06 15:01:32 +0200

    Page transitions and text animations

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
   <main class="home">
-    <background-blobs />
+
+    <transition appear name="fade">
+      <background-blobs />
+    </transition>  
+
     <div class="home_container">
       <div class="brand">
-        <h1 class="brand__title">iunctio</h1>
-        <h3 class="brand__subtitle">NETWORK VISUALISER</h3>
+
+        <transition appear name="bounce">
+          <h1 class="brand__title">iunctio</h1>
+        </transition>
+
+        <transition appear name="slide">
+          <h3 class="brand__subtitle">NETWORK VISUALISER</h3>
+        </transition>  
+
       </div>
       <router-link class="cta-visualise" :to="{ name: 'Visualise' }"
         >VISUALISE</router-link
@@ -27,3 +38,59 @@ export default defineComponent({
 </script>
 
 <style lang="scss" src="@/assets/styles/home.scss"></style>
+
+
+<style>
+  .slide-enter-active {
+    animation: coming 1s;
+    animation-delay: 2.175s;
+    opacity: 0;
+  }
+
+  @keyframes coming {
+    from {
+      transform: translateX(-100%);
+      opacity: 0; 
+    }
+    to {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  }
+
+  .bounce-enter-active{
+    animation: bounce-in .7s ease-in;
+    animation-delay: 1.3s;
+    opacity: 0;
+  }
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+
+    50% {
+      transform: scale(1.2);
+    }
+
+    100% {
+      transform: scale(1);
+      opacity: 1;
+    }
+  }
+
+  .fade-enter-active {
+    animation: fadeIn .4s ease-in;
+    animation-delay: 1s;
+    opacity: 0;
+  }
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0; 
+    }
+    to {
+      opacity: 1;
+    }
+  }
+</style>
