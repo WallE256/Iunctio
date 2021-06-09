a5c62e0d389eed5e4ab089bc8c6ec973aa124adc
Radu Pogonariu <r.pogonariu@student.tue.nl>
2021-06-09 10:41:44 +0200

    feat: Add sorting function based on date

--- src/scripts/sorter.ts
+++ src/scripts/sorter.ts
@@ -1,5 +1,21 @@
 import Graph from "graphology";
 
+export function sortEdgesByDate(graph: Graph): Map<string, string[]> {
+  const sorted = new Map();
+  for (const [node, attributes] of graph.nodeEntries()) {
+    const edges = graph.edges(node);
+    edges.sort(function (a, b) {
+      const dateA = graph.getEdgeAttribute(a, "date");
+      const dateB = graph.getEdgeAttribute(b, "date");
+      if (dateA < dateB) return -1;
+      else if (dateA == dateB) return 0;
+      else return 1;
+    });
+    sorted.set(node, edges);
+  }
+  return sorted;
+}
+
 export function sortNodesByCommunity(graph: Graph): string[] {
   const result = graph.nodes();
   result.sort(function (a, b) {
