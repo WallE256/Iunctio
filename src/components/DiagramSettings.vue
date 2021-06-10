cd20a8cb702e94e0f60074806c2b2c6c44a0945a
w2ptr <43069332+w2ptr@users.noreply.github.com>
2021-06-10 19:55:31 +0200

    Separate setting config from UploadDataset.vue and DiagramSettings.vue
    They're now in @/scripts/settingconfig.ts

--- src/components/DiagramSettings.vue
+++ src/components/DiagramSettings.vue
@@ -22,13 +22,7 @@ import CheckboxSetting from "@/components/settings/CheckboxSetting.vue";
 import NumberSetting from "@/components/settings/NumberSetting.vue";
 import SelectSetting from "@/components/settings/SelectSetting.vue";
 import TextSetting from "@/components/settings/TextSetting.vue";
-
-type SettingConfig = {
-  id: string,
-  component: string,
-  name: string,
-  properties?: any,
-};
+import { Setting, getVisibleSettings } from "@/scripts/settingconfig";
 
 export default defineComponent({
   components: {
@@ -47,7 +41,7 @@ export default defineComponent({
 
   data() {
     return {
-      settings: [] as SettingConfig[],
+      settings: [] as Setting[],
     };
   },
 
@@ -63,68 +57,7 @@ export default defineComponent({
       return;
     }
 
-    // see UploadDataset.vue for setting default values
-    switch (diagram.type) {
-      case "ArcDiagram":
-        this.settings = [
-          { id: "variety", component: "SelectSetting", name: "Node-Link Diagram Variety", properties: {
-            options: [ "circle", "line" ],
-            value: diagram.settings.variety,
-          } },
-          { id: "hoverEdgeDirection", component: "SelectSetting", name: "Edge Direction", properties: {
-            options: [ "incoming", "outgoing", "both" ],
-            value: diagram.settings.hoverEdgeDirection,
-          } },
-        ];
-        break;
-
-      case "SunburstDiagram":
-        this.settings = [
-          { id: "variety", component: "SelectSetting", name: "Hierarchical Diagram Variety", properties: {
-            options: [ "sunburst", "flame", "inverse-flame" ],
-            value: diagram.settings.variety,
-          } },
-          { id: "root", component: "SelectSetting", name: "Root Node", properties: {
-            options: ["[no root]"].concat(graph.nodes()), // TODO this has to show a proper name instead of IDs
-            value: diagram.settings.root,
-          } },
-          { id: "edgeType", component: "SelectSetting", name: "Edge Direction", properties: {
-            options: [ "incoming", "outgoing", "both" ],
-            value: diagram.settings.edgeType,
-          } },
-          { id: "height", component: "NumberSetting", name: "Layer Count", properties: {
-            min: 2,
-            max: 10,
-            value: diagram.settings.height,
-          } },
-          { id: "colourType", component: "SelectSetting", name: "Colour Determined By", properties: {
-            options: [ "rainbow" ].concat(Object.keys(graph.getNodeAttributes(graph.nodes()[0]))),
-            value: diagram.settings.colourType,
-          } },
-          { id: "minRenderSize", component: "NumberSetting", name: "Minimum Node Size 1/x", properties: {
-            min: 1,
-            value: diagram.settings.minRenderSize,
-          } },
-        ];
-        break;
-
-      case "DistributionDiagram":
-        this.settings = [
-          { id: "variety", component: "SelectSetting", name: "Diagram Variety", properties: {
-            options: [ "distribution", "histogram"],
-            value: diagram.settings.variety,
-          } },
-          { id: "logarithmic", component: "CheckboxSetting", name: "Logarithmic", properties: {
-            value: diagram.settings.logarithmic,
-          } },
-        ];
-        break;
-
-      default:
-        console.warn("Non-existent diagram type:", diagram.type);
-        this.settings = [];
-        break;
-    }
+    this.settings = getVisibleSettings(diagram, graph);
   },
 
   methods: {
