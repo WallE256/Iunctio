88339162823dd31d99f6f398b1cc6fd18cdeb950
w2ptr <43069332+w2ptr@users.noreply.github.com>
2021-06-24 14:46:25 +0200

    Improve ArcDiagram's look and move some generic code to util.ts

--- src/scripts/util.ts
+++ src/scripts/util.ts
@@ -1,5 +1,26 @@
 
+import { Dataset } from "@/scripts/globalstorage";
+
 export function dateIsBetween(date: string, timeRange: [string, string]): boolean {
   const d = new Date(date);
   return d >= new Date(timeRange[0]) && d <= new Date(timeRange[1]);
+}
+
+export function findMinMaxDates(dataset: Dataset): [Date, Date] {
+  let min = "9999-12-31";
+  let max = "1000-01-01";
+  const graph = dataset.graph;
+  const sortedEdges = dataset.getSortedEdges();
+  graph.forEachNode((node: string) => {
+    const list = sortedEdges.get(node);
+    // only have to look at the first and the last one
+    if (list && list.length > 0) {
+      const firstDate = graph.getEdgeAttribute(list[0], "date");
+      if (firstDate < min) min = firstDate;
+
+      const lastDate = graph.getEdgeAttribute(list[list.length - 1], "date");
+      if (lastDate > max) max = lastDate;
+    }
+  });
+  return [new Date(min), new Date(max)];
 }
