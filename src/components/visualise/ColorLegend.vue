82b91dc86a4ecda289812be5089135e961e00c1f
NeaGogu <v.plaiasu@student.tue.nl>
2021-06-26 22:30:28 +0300

    add legend component to arcdiagram

---
+++ src/components/visualise/ColorLegend.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="showLegend=!showLegend">Legend</button>
+  <div v-if="showLegend">
+    <div v-for="entry in colorScheme" :key="entry[0]">
+      <h1>{{entry[0]}} {{entry[1].assignedColor}}</h1>
+      
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ColorLegend",
+  props: ['colorScheme'],
+  data() {
+    return {
+      showLegend: false
+    }
+  },
+
+  
+})
+
+</script>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
